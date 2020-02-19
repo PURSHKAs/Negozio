70445f19855457cc147c4b06080ba789c2d17434
PURSHKA <59653447+PURSHKA@users.noreply.github.com>
2020-02-19 17:38:28 +0100

    Add files via upload

---
+++ src/ArticoloDAO.java
@@ -0,0 +1,109 @@
+package negozio;
+	import java.sql.*;
+	import java.util.ArrayList;
+
+	public class ArticoloDAO {
+		
+		private Connection con;
+		
+		public ArticoloDAO(Connection c) {
+			con = c;
+		}
+		
+		public ArrayList<Articolo> getArticoloByNome(String Nome) {
+			
+			ResultSet result;
+			Articolo A;
+			ArrayList<Articolo> ap = new ArrayList<Articolo>();
+			
+			String query = "select * from articolo where nome=?";
+			
+			try {
+				PreparedStatement pst = con.prepareStatement(query);
+				pst.setString(1,Nome);
+				result = pst.executeQuery();
+				
+					while(result.next()) {
+						A = new Articolo();
+						A.setIDArticolo(result.getString(1));
+						A.setNome(result.getString(2));
+						A.setTaglia(result.getString(3));
+						A.setColore(result.getString(4));
+						A.setScorte(result.getInt(5));
+						A.setPrezzo(result.getDouble(6));
+						ap.add(A);
+					}
+					return ap;
+				
+			} catch (SQLException e) {e.printStackTrace(); return null;}
+
+		}
+		
+public ArrayList<Articolo> getArticoloByID(String id) {
+			
+			ResultSet result;
+			Articolo A;
+			ArrayList<Articolo> ap = new ArrayList<Articolo>();
+			
+			String query = "select * from articolo where id=?";
+			
+			try {
+				PreparedStatement pst = con.prepareStatement(query);
+				pst.setString(1,id);
+				result = pst.executeQuery();
+				
+					while(result.next()) {
+						A = new Articolo();
+						A.setIDArticolo(result.getString(1));
+						A.setNome(result.getString(2));
+						A.setTaglia(result.getString(3));
+						A.setColore(result.getString(4));
+						A.setScorte(result.getInt(5));
+						A.setPrezzo(result.getDouble(6));
+						ap.add(A);
+					}
+					return ap;
+				
+			} catch (SQLException e) {e.printStackTrace(); return null;}
+
+		}
+		
+		public void AggiungiArticolo(String nome, String taglia, String colore, int scorte, double prezzo){
+			String query = "INSERT INTO articolo (nome, taglia, colore, scorta, prezzo) VALUES (?,?,?,?,?)";
+			
+			try {
+				PreparedStatement pst = con.prepareStatement(query);
+				pst.setString(1, nome);
+				pst.setString(2, taglia);
+				pst.setString(3, colore);
+				pst.setInt(4, scorte);
+				pst.setDouble(5, prezzo);
+				pst.executeUpdate();
+			}catch(SQLException e) {e.printStackTrace();}
+		}
+		
+		public void RimuoviArticolo(int ID) {
+			String query = "delete from articolo where idarticolo = ?";
+			try {
+				PreparedStatement pst = con.prepareStatement(query);
+				pst.setInt(1, ID);
+				pst.executeUpdate();
+			}catch (SQLException e) {e.printStackTrace();}
+		}
+		
+		
+		public void ModificaArticolo(String nome, String taglia, String colore, int scorte, double prezzo) {
+			String query = "UPDATE articolo SET nome=?, taglia=?, colore=?, scorta=? , prezzo=? WHERE id=?";
+			try {
+				PreparedStatement pst = con.prepareStatement(query);
+				pst.setString(1, nome);
+				pst.setString(2, taglia);
+				pst.setString(3, colore);
+				pst.setInt(4, scorte);
+				pst.setDouble(5, prezzo);
+				pst.executeUpdate();
+			}catch(SQLException e) {e.printStackTrace();}
+		}
+	}
+
+
